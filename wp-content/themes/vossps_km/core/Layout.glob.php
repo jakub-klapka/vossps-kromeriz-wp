eefe8e113b9d446f442d705bb4797c39605c18d0
Jakub Klapka <klapka@lumiart.cz>
2015-06-24 19:22:26 +0200

    Fixed cachebusting on post delete

--- wp-content/themes/vossps_km/core/Layout.glob.php
+++ wp-content/themes/vossps_km/core/Layout.glob.php
@@ -10,6 +10,26 @@ class Layout {
 	
 		add_theme_support( 'menus' );
 
+		add_action( 'delete_post', array( $this, 'delete_cache_on_save_post' ) );
+
+		/**
+		 * ACF cache delete
+		 */
+		add_filter( 'acf/save_post', function($post_id) {
+			if( $post_id === 'options' ) {
+				if( function_exists( 'wp_cache_clear_cache' ) ){
+					wp_cache_clear_cache();
+				}
+			}
+		} );
+
+	}
+
+	public function delete_cache_on_save_post( $post_id = null )
+	{
+		if( function_exists('wp_cache_clear_cache') ){
+			wp_cache_clear_cache();
+		}
 	}
 
 }
